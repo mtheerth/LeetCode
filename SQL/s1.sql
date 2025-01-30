907668f7ecad52393f5fcad81d87543337d18ce5
Annapurna Theerthala <annapurna.theerthala@gmail.com>
2025-01-29 16:43:36 -0800

    multi comment lines mistaken

--- SQL/s1.sql
+++ SQL/s1.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-''' find the ids of products that are both low fat and recyclable any order.
+/* find the ids of products that are both low fat and recyclable any order.
 Products
 
 +-------------+---------+
@@ -9,5 +9,5 @@ Products
 | low_fats    | enum    |
 | recyclable  | enum    |
 +-------------+---------+
-'''
+*/
 SELECT product_id from Products where low_fats='Y' and recyclable='Y';
