0cd703c5586f2856a74e3ee8b1a1b8a30ea2dd26
Annapurna Theerthala <annapurna.theerthala@gmail.com>
2025-02-01 13:08:41 -0800

    LC Medium 11. Container With Most Water

---
+++ Java/MaxWaterContainer.java
@@ -0,0 +1,18 @@
+class MaxWaterContainer {
+    public int maxArea(int[] height) {
+        int output =0 ;
+        int left = 0;
+        int right = height.length -1;
+        while (left < right){
+            int val1 = Math.min(height[left], height[right]);
+            int val2 = right - left;
+            output = Math.max(output, val1 * val2);
+            if (height[left] < height[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return output;
+    }
+}
