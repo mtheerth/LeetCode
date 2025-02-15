48813e45c7cfcf36befdb88bae294d174e6a8c86
Annapurna Theerthala <annapurna.theerthala@gmail.com>
2025-02-15 13:23:08 -0800

    LC Medium 1143. Longest Common Subsequence

---
+++ Java/DynamicProgramming/LongestCommonSubsequence.java
@@ -0,0 +1,36 @@
+package practise.leetcode;
+
+import java.util.*;
+
+public class LongestCommonSubsequence {
+
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        // Create a 2D DP table to store the lengths of LCS
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                // If characters match, add 1 to the result of the previous subproblem
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    // Otherwise, take the maximum of the two subproblems
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // The bottom-right cell contains the length of the LCS
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String text1 = "abcde";
+        String text2 = "ace";
+        System.out.println("Length of LCS: " + longestCommonSubsequence(text1, text2)); // Output: 3
+    }
+}
