d2b9cfac1fbf494a88cd04b1a0b40becc92b5d6e
Annapurna Theerthala <annapurna.theerthala@gmail.com>
2025-02-20 20:29:18 -0800

    LC Medium Generate Parentsis

---
+++ Java/DynamicProgramming/GenerateParentsis.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class GenerateParentsis {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+    
+    private void backtrack(List<String> result, String current, int openCount, int closeCount, int n) {
+        if (openCount == n && closeCount == n) {
+            result.add(current);
+            return;
+        }
+        
+        if (openCount < n) {
+            backtrack(result, current + "(", openCount + 1, closeCount, n);
+        }
+        if (closeCount < openCount) {
+            backtrack(result, current + ")", openCount, closeCount + 1, n);
+        }
+    }
+    public static void main(String[] args) {
+        GenerateParentsis solution = new GenerateParentsis();
+        System.out.println(solution.generateParenthesis(3));
+        // Output: ["((()))","(()())","(())()","()(())","()()()"]
+    }
+}
+
