bb2f0f1f04e5281f8ccdd8b57b59032afd272947
Annapurna Theerthala <annapurna.theerthala@gmail.com>
2025-03-03 20:33:55 -0800

    LC Medium 199. Binary Tree Right Side View

---
+++ Java/AmazonMedium/BinaryRightSide.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> output = new ArrayList<Integer>();
+        if (root == null){
+            return output;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int level = q.size();
+            for (int i=0 ; i < level; i++){
+            TreeNode currentNode = q.poll();
+            if (i == level -1){ // for each iteration q size is 2 and we loop over 0,1 and 2-1 ==1 i.e right side ignoring left which is 2-2 == 0.
+            output.add(currentNode.val);
+            }
+            if (currentNode.left != null){
+                q.offer(currentNode.left);
+            } 
+            if (currentNode.right != null){
+                q.offer(currentNode.right); 
+            } 
+
+            }
+        }
+        return output;
+    }
+}
