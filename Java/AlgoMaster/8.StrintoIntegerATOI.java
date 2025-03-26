2b61fef186ed38e31ae3c7166f4284fb88c5c0ba
Annapurna Theerthala <annapurna.theerthala@gmail.com>
2025-03-25 23:15:25 -0700

    LC Hard String ATOI

---
+++ Java/AlgoMaster/8.StrintoIntegerATOI.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int myAtoi(String s) {
+       if (s == null || s.isEmpty()) return 0;
+
+        int i = 0, n = s.length(), sign = 1, result = 0;
+        
+        // Step 1: Ignore leading whitespaces
+        while (i < n && s.charAt(i) == ' ') i++;
+
+        // Step 2: Check for '+' or '-'
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+        int threshold = Integer.MAX_VALUE  / 10;
+        // Step 3: Convert digits and check for overflow
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            // Check for overflow before adding digit
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                //if (result > threshold || (result == threshold && currentChar > '7')) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            result = result * 10 + digit;
+            i++;
+        }
+
+        return result * sign;
+    }
+}
